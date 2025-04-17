f8bb45c6c09bc3c4011e651425fc09b357ac9923
Maria Samodelova <samodelovamari@gmail.com>
2025-04-17 12:17:37 +0300

    initial commit of the project

--- src/HashMap.java
+++ src/HashMap.java
@@ -1,4 +1,107 @@
-package PACKAGE_NAME;
-
 public class HashMap {
+    private static final int INITIAL_CAPACITY = 16;
+    private Entry[] buckets;
+
+    public HashMap() {
+        buckets = new Entry[INITIAL_CAPACITY];
+    }
+
+    private int hash(Object key) {
+        return key.hashCode() % buckets.length;
+    }
+
+    /**
+     * Метод put().
+     * Добавляет значение в HashMap с указанным ключом.
+     * @param key ключ, по которому необходимо добавить значение
+     * @param value значение для добавления
+     */
+
+
+    public void put(Object key, Object value) {
+        int index = hash(key);
+        Entry entry = new Entry(key, value);
+        if (buckets[index] == null) {
+            buckets[index] = entry;
+        } else {
+            Entry current = buckets[index];
+            while (current.next != null) {
+                if (current.key.equals(key)) {
+                    current.value = value;
+                    return;
+                }
+                current = current.next;
+            }
+            current.next = entry;
+        }
+    }
+
+    /**
+     * Метод get().
+     * Получает значение из HashMap по указанному ключу.
+     * @param key ключ, по которому необходимо получить значение
+     * @return значение, соответствующее указанному ключу, или null, если ключ не найден
+     */
+
+
+    public Object get(Object key) {
+        int index = hash(key);
+        Entry current = buckets[index];
+        while (current != null) {
+            if (current.key.equals(key)) {
+                return current.value;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    /**
+     * Метод remove().
+     * Удаляет значение из HashMap по указанному ключу.
+     * @param key ключ, по которому необходимо удалить значение
+     */
+
+    public void remove(Object key) {
+        int index = hash(key);
+        Entry current = buckets[index];
+        Entry prev = null;
+        while (current != null) {
+            if (current.key.equals(key)) {
+                if (prev == null) {
+                    buckets[index] = current.next;
+                } else {
+                    prev.next = current.next;
+                }
+                return;
+            }
+            prev = current;
+            current = current.next;
+        }
+    }
+
+    private static class Entry {
+        Object key;
+        Object value;
+        Entry next;
+
+        Entry(Object key, Object value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key1", "newvalue1");
+
+        System.out.println("Value for key 'key1': " + map.get("key1"));
+        System.out.println("Value for key 'key2': " + map.get("key2"));
+
+        map.remove("key1");
+        System.out.println("After removing key 'key1', value for key 'key1': " + map.get("key1"));
+    }
 }
--- src/HashSet.java
+++ src/HashSet.java
@@ -1,4 +1,78 @@
-package PACKAGE_NAME;
-
 public class HashSet {
+    private static final int INITIAL_CAPACITY = 16;
+    private Object[] buckets;
+    private int size;
+
+    public HashSet() {
+        buckets = new Object[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    private int hash(Object key) {
+        return key.hashCode() % buckets.length;
+    }
+
+    /**
+     * Метод add().
+     * Добавляет элемент в HashSet, если его там еще нет.
+     * @param key элемент для добавления
+     */
+
+    public void add(Object key) {
+        int index = hash(key);
+        if (buckets[index] == null) {
+            buckets[index] = key;
+            size++;
+        }
+    }
+
+    /**
+     * Метод contains().
+     * Проверяет, содержит ли HashSet указанный элемент.
+     * @param key элемент для проверки наличия
+     * @return true, если HashSet содержит указанный элемент, иначе false
+     */
+
+    public boolean contains(Object key) {
+        int index = hash(key);
+        return buckets[index] != null && buckets[index].equals(key);
+    }
+
+    /**
+     * Метод remove().
+     * Удаляет элемент из HashSet.
+     * @param key элемент для удаления
+     */
+
+    public void remove(Object key) {
+        int index = hash(key);
+        if (buckets[index] != null && buckets[index].equals(key)) {
+            buckets[index] = null;
+            size--;
+        }
+    }
+
+    /**
+     * Метод size().
+     * Возвращает количество элементов в HashSet.
+     * @return количество элементов в HashSet
+     */
+
+    public int size() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+        HashSet set = new HashSet();
+        set.add("apple");
+        set.add("banana");
+        set.add("apple");
+
+        System.out.println("Set size: " + set.size());
+        System.out.println("Set contains 'apple': " + set.contains("apple"));
+        System.out.println("Set contains 'banana': " + set.contains("banana"));
+
+        set.remove("apple");
+        System.out.println("After removing 'apple', set contains 'apple': " + set.contains("apple"));
+    }
 }
